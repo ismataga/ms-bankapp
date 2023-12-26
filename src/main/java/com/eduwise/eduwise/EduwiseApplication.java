8cf05fdfd18ddca51d025b11c7b73db264cfaba3
Ismat Aghamirzayev <ismataga1@gmail.com>
2023-12-26 09:10:59 +0400

    initial commit

---
+++ src/main/java/com/eduwise/eduwise/EduwiseApplication.java
@@ -0,0 +1,13 @@
+package com.eduwise.eduwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduwiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduwiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduwise/eduwise/EduwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduwise.eduwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
