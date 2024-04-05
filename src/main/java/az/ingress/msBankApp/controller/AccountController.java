ef4b9a73ae55fa553eb981de32242932221f0e68
Ismat Aghamirzayev <ismeta@goldenpay.az>
2024-04-05 22:55:56 +0400

    add AccountController

--- src/main/java/az/ingress/msBankApp/controller/AccountController.java
+++ src/main/java/az/ingress/msBankApp/controller/AccountController.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.JstlUtils;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/students")
 public class AccountController {
+
 }
